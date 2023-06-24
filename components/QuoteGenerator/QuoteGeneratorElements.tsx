ca5b8a11ade7261d9662eda76a733709dc147968
Eugene Tye <eugenetye123@gmail.com>
2023-06-23 21:39:05 -0400

    added images and footer

--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -20,3 +20,33 @@ export const GradientBackgroundCon = styled.div`
         }
     }
 `;
+
+export const BackgroundImage1 = styled(Image)`
+    position: relative;
+    z-index: 1;
+    margin-left: -10px;
+    margin-top: -10px;
+`;
+
+export const BackgroundImage2 = styled(Image)`
+    position: fixed;
+    z-index: 1;
+    right: -120px;
+    bottom: -10px;
+`;
+
+export const FooterCon = styled.div`
+    width: 100vw;
+    height: 50px;
+    text-align: center;
+    font-family: 'Source Code Pro', monospace;
+    font-size: 15px;
+    position: absolute;
+    bottom: 0;
+    color: white;
+    z-index: 999999;
+`;
+
+export const FooterLink = styled(Link)`
+    color: white;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import Image from 'next/image'
 import Head from 'next/head'
-import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+import React, { useState } from 'react'
+
+//Components
+import { GradientBackgroundCon, BackgroundImage1, BackgroundImage2, FooterCon, FooterLink } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+// Assets
+import Clouds1 from '@/assets/cloud-and-thunder.png'
+import Clouds2 from '@/assets/cloudy-weather.png'
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -13,7 +22,32 @@ export default function Home() {
       </Head>
       {/* Background */}
       <GradientBackgroundCon>
-        
+
+      
+
+
+      {/* Background Images */}
+      <BackgroundImage1
+        src = {Clouds1}
+        height = "300"
+        alt = "cloudybackground1"
+      />
+      <BackgroundImage2
+        src = {Clouds2}
+        height = "300"
+        alt = "cloudybackground2"
+      />
+
+      {/* Footer */}
+      <FooterCon>
+        <>
+          Quotes Generated: {numberOfQuotes}
+          <br/>
+          Developed by <FooterLink href = "https://eugenetye.com/" 
+          target="_blank" rel="noopener and noreferrer"> Eugene Tye</FooterLink>
+        </>
+      </FooterCon>
+
       </GradientBackgroundCon>
     </>
   )
