4fc4ebda0177aa5606f4de55542a4f7b5a02f202
Eugene Tye <eugenetye123@gmail.com>
2023-06-23 21:08:38 -0400

    add gradient background colour

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  compiler : {
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+      fileName: true,
+      topLevelImportPaths: [],
+      meaninglessFileNames: ["index"],
+      cssProp: true,
+      namespace: "",
+      minify: false,
+      transpileTemplateLiterals: false,
+      pure: false,
+    },
+  },
+}
+
+module.exports = nextConfig
